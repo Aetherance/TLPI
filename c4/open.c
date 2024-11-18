0c3f9014d553747e4630aaf770e36386b8c03580
User <2085163736@qq.com>
2024-11-18 21:00:07 +0800

    add files

--- c4/open.c
+++ c4/open.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<stdlib.h>
+#include<unistd.h>
 int main()
 {
-    fopen("TF/0.txt",)
+    int fd = open("/home/user/CODE/TLPI/TF/0.txt",O_RDWR);
+    FILE* fp = fdopen(fd,"r+w");
+    char * str = (char *)malloc(1280);
+    fscanf(fp,"\n%[^\n]",str);
+    printf("%s\n",str);
+    fscanf(fp,"\n%[^\n]",str);
+    printf("%s",str);
 
-
-
-
-
-
-
+    close(fd);
+    free(fp);
     return 0;
 }
