f29e1d6df8845fb092d59c47c20b41b8968373b5
User <2085163736@qq.com>
2024-11-21 16:50:05 +0800

    update files

--- c18/ls.c
+++ c18/ls.c
@@ -7,6 +7,11 @@ int main(int argc,char ** argv)
     if(argc==1)argv[1] = ".";
     struct dirent * temp;
     DIR* dir = opendir(argv[1]);
+    if(dir==NULL)
+    {   
+        printf("ls: 无法访问 '%s': 没有那个文件或目录\n",argv[1]);
+        return 1;
+    }
     struct stat* temp_stat;
     char * temp_name;
     while((temp = readdir(dir))!=NULL)
@@ -14,7 +19,7 @@ int main(int argc,char ** argv)
         if(!strcmp(temp->d_name,".")||!strcmp(temp->d_name,".."))continue;
         sprintf(temp_name,"./%s",temp->d_name);
         stat(temp_name,temp_stat);
-        if(S_ISREG(temp_stat->st_mode))printf("%s ",temp->d_name);
+        if(S_ISREG(temp_stat->st_mode))printf("%s  ",temp->d_name);
         if(S_ISDIR(temp_stat->st_mode))printf("\033[;1;34m%s \033[0m",temp->d_name);
     }
 
