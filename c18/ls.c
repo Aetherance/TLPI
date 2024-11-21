f79ba6fcf76c2160efbb94073fd77998a80975ee
User <2085163736@qq.com>
2024-11-21 17:49:17 +0800

    update files

--- c18/ls.c
+++ c18/ls.c
@@ -19,7 +19,8 @@ int main(int argc,char ** argv)
         if(!strcmp(temp->d_name,".")||!strcmp(temp->d_name,".."))continue;
         sprintf(temp_name,"./%s",temp->d_name);
         stat(temp_name,temp_stat);
-        if(S_ISREG(temp_stat->st_mode))printf("%s  ",temp->d_name);
+        if(S_ISREG(temp_stat->st_mode)&&!(temp_stat->st_mode & S_IXUSR))printf("%s  ",temp->d_name);
+        if(S_ISREG(temp_stat->st_mode)&& (temp_stat->st_mode & S_IXUSR))printf("\033[;1;32m%s  \033[0m",temp->d_name);
         if(S_ISDIR(temp_stat->st_mode))printf("\033[;1;34m%s \033[0m",temp->d_name);
     }
 
