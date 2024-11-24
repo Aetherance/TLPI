1555993ed0867ad731147740a4d6685ae7f91974
User <2085163736@qq.com>
2024-11-24 20:22:20 +0800

    update files

--- c18/ls(failed).c
+++ c18/ls(failed).c
@@ -5,8 +5,9 @@
 #include<stdlib.h>
 int main(int argc,char ** argv)
 {
+    //argv[1] = ".";
+    printf("%s",argv[1]);
     if(argc==1)argv[1] = ".";
-    argv[1] = "..";
     struct dirent * temp;
     DIR* dir = opendir(argv[1]);
     if(dir==NULL)
@@ -15,12 +16,12 @@ int main(int argc,char ** argv)
         return 1;
     }
     struct stat* temp_stat;
-    char * temp_name;
-    char * path;
+    char temp_name[1000];
+    char path[1000];
     while((temp = readdir(dir))!=NULL)
     {
         if(!strcmp(temp->d_name,".")||!strcmp(temp->d_name,".."))continue;
-        //sprintf(temp_name,"%s",temp->d_name);
+        sprintf(temp_name,"%s/%s",argv[1],temp->d_name);
         realpath(temp->d_name,path);
         stat(path,temp_stat);
         if(S_ISREG(temp_stat->st_mode)&&!(temp_stat->st_mode & S_IXUSR))printf("%s  ",temp->d_name);
@@ -28,6 +29,6 @@ int main(int argc,char ** argv)
         if(S_ISDIR(temp_stat->st_mode))printf("\033[;1;34m%s  \033[0m",temp->d_name);
     }
 
-    printf("\n");
+    // printf("\n");
     return 0;
 }
