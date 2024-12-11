245b884b992c52dac5977c0133db1f95a4ce2617
User <2085163736@qq.com>
2024-12-11 12:09:07 +0800

    up

---
+++ c24/1.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+
+vector<string> split(string s)
+{
+    vector<string>result;
+    int pos = 0;
+    while (pos + 1 < s.size())
+    {
+        int n = 0;
+        while (s[pos+n]!=' '&&pos+n<s.size())
+        {
+            n++;
+        }
+        result.push_back(s.substr(pos,n));
+        pos += n + 1;
+    }
+    
+    return result;
+}
+int main()
+{
+    string in;
+    getline(cin,in);
+    vector<string>v;
+    v = split(in);
+    for(int i = 0;i<v.size();i++)
+        cout<<v[i]<<endl;
+    return 0;
+}
